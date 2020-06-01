c7cc843b3765a2d1df4759ec60c129a74f117396
tofaelkabir <tofaelkabir@gmail.com>
2020-05-31 22:58:58 -0400

    05312020 11pm

---
+++ src/javaBasics/HelloWorld_new.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class HelloWorld_new {
+
+	public static void main(String[] args) { // This is main method
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/javaBasics/JavaComments.java
@@ -0,0 +1,15 @@
+package javaBasics;
+/*
+Java Comments:
+This is multiple line comments 
+The Java comments are the statements that are not executed by the compiler and interpreter. The comments can be used to provide information or explanation about the variable, method, class or any statement. It can also be used to hide program code.
+*/
+
+public class JavaComments {
+
+	public static void main(String[] args) { // Single line comments
+		System.out.println("This class is about Java comments");
+
+	}
+
+}
